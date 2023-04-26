16bd60aef7e5e4680260ffcbd084a4ed343793d0
Hakan <hakan@kurtulus.dev>
2023-04-26 16:48:37 +0200

    - Add external SSH Keys

--- modules/aws-eu-central-1/bootstrap.tf
+++ modules/aws-eu-central-1/bootstrap.tf
@@ -4,4 +4,9 @@ module "bootstrap" {
   instance_public_ip = aws_instance.node.public_ip
   ssh_username       = "admin"
   k3s_role           = "leader"
+  k3s_leader_endpoint = "${aws_instance.node.public_ip}:6443"
+
+  depends_on = [
+    aws_instance.node
+  ]
 }
--- modules/az-japaneast/bootstrap.tf
+++ modules/az-japaneast/bootstrap.tf
@@ -3,4 +3,9 @@ module "bootstrap" {
 
   instance_public_ip = azurerm_linux_virtual_machine.node.public_ip_address
   ssh_username = "azureuser"
+  k3s_leader_endpoint = "${azurerm_linux_virtual_machine.node.public_ip_address}:6443"
+
+  depends_on = [
+    azurerm_linux_virtual_machine.node
+  ]
 }
