de23b73607b9b457eac10667878362be6d2ef360
Hakan <hakan@kurtulus.dev>
2023-04-26 17:24:58 +0200

    - Add external SSH Keys

--- modules/bootstrap/scripts/install.sh
+++ modules/bootstrap/scripts/install.sh
@@ -11,6 +11,6 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export LANGUAGE=C.UTF-8
 
-# update system and install wireguard
+# update system and install dependencies
 sudo apt update
 sudo apt install -y wireguard jq
