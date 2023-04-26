be1ea4ea04ac4dc681ad9a5b0aa175774ff770ca
Hakan <hakan@kurtulus.dev>
2023-04-26 10:15:01 +0200

    - Add external SSH Keys

--- modules/gcp-us-central1/ssh_keys.tf
+++ modules/gcp-us-central1/ssh_keys.tf
@@ -3,4 +3,9 @@ module "ssh_keys" {
 
   instance_public_ip = google_compute_address.static_ip.address
   ssh_username = "tf-serviceaccount"
+
+  depends_on = [
+    google_compute_address.static_ip,
+    google_compute_instance.node
+  ]
 }
