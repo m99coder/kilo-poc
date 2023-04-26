8d42cdbb8ef05a1c29bbda36594d3379e11639f2
Hakan <hakan@kurtulus.dev>
2023-04-26 10:58:22 +0200

    - Add external SSH Keys

--- modules/bootstrap/scripts/install.sh
+++ modules/bootstrap/scripts/install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-# configure system
-export PS1="\[\e[0;32m\][\u@\h \W]$\[\e[m\] "
+echo "Hello!"
 
-#locale -a
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-export LANGUAGE=C.UTF-8
-
-# update system and install wireguard
-sudo apt update
-sudo apt install -y wireguard
+## configure system
+#export PS1="\[\e[0;32m\][\u@\h \W]$\[\e[m\] "
+#
+##locale -a
+#export LC_ALL=C.UTF-8
+#export LANG=C.UTF-8
+#export LANGUAGE=C.UTF-8
+#
+## update system and install wireguard
+#sudo apt update
+#sudo apt install -y wireguard
